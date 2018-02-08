126a4b0a7f00449bc1a80e86bf239b98297334f1
JonNieblas <32368944+JonNieblas@users.noreply.github.com>
2018-02-08 12:48:32 -0700

    Delete Opcode.java
    No need for this interface, changing design to methods within PALParser for now.

--- src/Opcode.java
+++
@@ -1,8 +0,0 @@
-/*
- * This interface will be used to make different types of opcode classes
- * they will be children of this parent
- * They will each have their own requirements for format
- */
-
-public interface Opcode {
-}
